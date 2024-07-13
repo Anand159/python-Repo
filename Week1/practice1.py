06b4d6986e4f44e53021bee1874d95a4e755babd
Anand159 <anandkumarsoni154@gmail.com>
2024-07-13 21:18:26 +0530

    practice1

--- Week1/practice1.py
+++ Week1/practice1.py
@@ -1,13 +1,17 @@
+#Practice 1:
+#1. Create a list of the first 10 square numbers.
+#Example: square number means [1,4,9,……..]
+#2. Replace the third element with 50.
+#3. Insert a new element 25 at the fifth position.
+#4. Remove the last element from the list.
+#5. Print the final list.
+
 square=[1,4,3,16,25,36,49,64,81,100]
-print("square is list :- ")
-print(square)
-print("Replace 3 element with 50")
+print("square is list :- ",square)
 square[2]=50
-print(square)
-print("inset new element")
+print("Replace 3 element with 50",square)
 square.insert(4,25)
-print(square)
-print("last element of list")
+print("inset new element",square)
 square.pop()
-print(square)
+print("last element of list",square)
 

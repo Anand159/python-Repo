f3b48fa52d16a8626e0a3532e89397608972c432
Anand159 <anandkumarsoni154@gmail.com>
2024-07-13 17:50:19 +0530

    practice1

---
+++ Week1/practice1.py
@@ -0,0 +1,13 @@
+square=[1,4,3,16,25,36,49,64,81,100]
+print("square is list :- ")
+print(square)
+print("Replace 3 element with 50")
+square[2]=50
+print(square)
+print("inset new element")
+square.insert(4,25)
+print(square)
+print("last element of list")
+square.pop()
+print(square)
+

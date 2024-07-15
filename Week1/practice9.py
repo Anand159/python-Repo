8ace658dce1bf5a7edb519cb4e7f8a2c5f444f73
Anand159 <anandkumarsoni154@gmail.com>
2024-07-15 14:43:21 +0530

    practice-9

---
+++ Week1/practice9.py
@@ -0,0 +1,15 @@
+#Practice 9:
+#Set Membership
+#1. Create a set of vowels.
+#2. Check if the letter 'e' is in the set.
+#3. Check if the letter 'x' is in the set.
+#4. Print the results.
+print("vowels :- ")
+vowel={"a","e","i","o","u"}
+print(vowel)
+letter="e" in vowel
+print("The letter 'e' is in the set :- ",letter)
+letter2="x" in vowel
+print("The letter 'x' is in the set :-",letter2)
+print("final result")
+print(vowel)

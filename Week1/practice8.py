84567d0fa86e191d92e1db3c380f09d2ab4c446b
Anand159 <anandkumarsoni154@gmail.com>
2024-07-15 14:30:08 +0530

    practice-8

---
+++ Week1/practice8.py
@@ -0,0 +1,23 @@
+#Practice 8:
+#Sets
+#1. Create a set of the first five prime numbers.
+#2. Add the number 11 to the set.
+#3. Remove the number 2 from the set.
+#4. Check if the number 7 is in the set.
+#5. Print the final set.
+print("Prime number :- ")
+num={1,2,3,5,7}
+print(num)
+num.add(11)
+print("Add number 11")
+print(num)
+print("Remove number 2 ")
+num.remove(2)
+print(num)
+check_num=7 in num
+print("number 7 present in the set :- ",check_num)
+print("final set")
+print(num)
+
+
+

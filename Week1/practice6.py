ccd90256f4eaf54e520a314c9186ccc34be092ef
Anand159 <anandkumarsoni154@gmail.com>
2024-07-15 14:08:04 +0530

    practice-6

---
+++ Week1/practice6.py
@@ -0,0 +1,10 @@
+#Practice 6:
+#Nested Tuples
+#1. Create a tuple that contains two nested tuples, each with three elements.
+#2. Access the second element of the first nested tuple.
+#3. Access the first element of the second nested tuple.
+#4. Print both elements
+
+print("nested")
+
+

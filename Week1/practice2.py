d5a88d4e1dffadfe12b3594fbf76bb43ae5f9375
Anand159 <anandkumarsoni154@gmail.com>
2024-07-15 11:09:55 +0530

    practice-2

--- Week1/practice2.py
+++ Week1/practice2.py
@@ -1,13 +1,11 @@
-#Practice 2:
-#List Slicing:
-#List slicing is a way to get parts (or slices) of a list. It's like cutting a piece out of a list, just like you
-#would cut a piece out of a cake.
-#TASK:
-#1. Create a list of the first 20 natural numbers.
-#2. Slice the list to get the first 10 elements.
-#3. Slice the list to get the last 5 elements.
-#4. Print both slices.
-
+print("\n")
+print("Practice 2:")
+print("TASK:")
+print("1. Create a list of the first 20 natural numbers.")
+print("2. Slice the list to get the first 10 elements.")
+print("3. Slice the list to get the last 5 elements.")
+print("4. Print both slices.")
+print("\n")
 natural=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 print("Natural Number :- ",natural)
 print("first 10 element slice",natural[:10])

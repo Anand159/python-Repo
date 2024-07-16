50a6f6265380dd02c740da515b1591c2430db56b
Anand159 <anandkumarsoni154@gmail.com>
2024-07-16 13:27:44 +0530

    practice1

--- Week1/practice1.py
+++ Week1/practice1.py
@@ -7,7 +7,7 @@ print("3. Insert a new element 25 at the fifth position.")
 print("4. Remove the last element from the list.")
 print("5. Print the final list.")
 print("\n")
-square=[1,4,3,16,25,36,49,64,81,100]
+square=[1,4,9,16,25,36,49,64,81,100]
 print("square is list :- ",square)
 square[2]=50
 print("Replace third element with 50",square)

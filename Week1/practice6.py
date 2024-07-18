c85895e08956c87e02179b28ce899d6935c014e0
Anand159 <anandkumarsoni154@gmail.com>
2024-07-18 13:15:32 +0530

    practice6

--- Week1/practice6.py
+++ Week1/practice6.py
@@ -1,16 +1,19 @@
-#Practice 6:
-#Nested Tuples
-#1. Create a tuple that contains two nested tuples, each with three elements.
-#2. Access the second element of the first nested tuple.
-#3. Access the first element of the second nested tuple.
-#4. Print both elements
-
+print("\n")
+print("Practice 6:")
+print("Nested Tuples")
+print("1. Create a tuple that contains two nested tuples, each with three elements.")
+print("2. Access the second element of the first nested tuple.")
+print("3. Access the first element of the second nested tuple.")
+print("4. Print both elements")
+print("\n")
 Tuples=((1,2,3),(2,4,6))
 print("nested tuples :- ",Tuples)
 access=Tuples[0][1]
-print(access)
+print("Access the second element of the first nested tuple :- ",access)
 acess2=Tuples[1][0]
-print(acess2)
+print("Access the first element of the second nested tuple :- ",acess2)
+
+
 
 
 
